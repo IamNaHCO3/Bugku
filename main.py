0841435c8bb896dc50f06426451e81f7e199c861
IamNaHCO3 <32425395+IamNaHCO3@users.noreply.github.com>
2024-04-12 12:35:59 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import ddddocr
+import requests
+
+
+class Bugku:
+    def __init__(self, username, password):
+        self.url = {
+            'index'  : 'https://ctf.bugku.com/',
+            'login'  : 'https://ctf.bugku.com/login/check.html',
+            'captcha': 'https://ctf.bugku.com/captcha.html',
+            'checkin': 'https://ctf.bugku.com/user/checkin'
+        }
+        self.username = username
+        self.password = password
+        self.is_login = False
+        self.session = requests.Session()
+
+    def login(self) -> requests.Response:
+        data = {
+            'username' : self.username,
+            'password' : self.password,
+            'vcode'    : self.get_captcha(),
+            'autologin': 1
+        }
+        self.session.headers['X-Requested-With'] = 'XMLHttpRequest'
+
+        response = self.session.post(url=self.url['login'], data=data)
+        while response.json()['msg'] == '验证码错误!':
+            data['vcode'] = self.get_captcha()
+            response = self.session.post(url=self.url['login'], data=data)
+
+        if response.json()['code'] == 1:
+            self.is_login = True
+            return response.json()
+        else:
+            raise response.json()['msg']
+
+    def get_captcha(self) -> str:
+        response = self.session.get(url=self.url['captcha'])
+        ocr = ddddocr.DdddOcr(show_ad=False)
+        res = ocr.classification(response.content)
+        return res
+
+    def checkin(self) -> str:
+        if not self.is_login:
+            self.login()
+        response = self.session.get(url=self.url['checkin'])
+        return response.json()['msg']
+
+
+bugku = Bugku(username='', password='')
+print(bugku.checkin())
